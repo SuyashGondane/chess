650c68b6618619f89b723878e7f8c98052bfa77f
SuyashGondane <suyash9350@gmail.com>
2022-07-07 12:48:49 +0530

    chess table ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChessTableComponent } from './components/chess-table/chess-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChessTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chess-table/chess-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-table',
+  templateUrl: './chess-table.component.html',
+  styleUrls: ['./chess-table.component.css']
+})
+export class ChessTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
